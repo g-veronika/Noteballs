c79f7bc040ab600e56337174abcfc5aa4d2204ae
g-veronika <g.veronika2021@gmail.com>
2022-12-26 16:24:34 +0100

    router setup

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [] 
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
