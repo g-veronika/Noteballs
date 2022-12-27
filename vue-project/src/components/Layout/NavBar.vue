067e2dd9f696882aa406c10baa65bb78ca5e3953
g-veronika <g.veronika2021@gmail.com>
2022-12-27 19:27:12 +0100

    NavBar navigation & Logo & rectified css

--- vue-project/src/components/Layout/NavBar.vue
+++ vue-project/src/components/Layout/NavBar.vue
@@ -5,9 +5,9 @@
         aria-label="main navigation"
     >
         <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-          </a>
+          <div class="navbar-item is-size-4 is-family-monospace">
+            Noteballs
+          </div>
       
           <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
@@ -17,50 +17,22 @@
         </div>
       
         <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">
-              Home
-            </a>
-      
-            <a class="navbar-item">
-              Documentation
-            </a>
-      
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
-                More
-              </a>
-      
-              <div class="navbar-dropdown">
-                <a class="navbar-item">
-                  About
-                </a>
-                <a class="navbar-item">
-                  Jobs
-                </a>
-                <a class="navbar-item">
-                  Contact
-                </a>
-                <hr class="navbar-divider">
-                <a class="navbar-item">
-                  Report an issue
-                </a>
-              </div>
+            <div class="navbar-end">
+                <RouterLink 
+                    to="/"
+                    class="navbar-item"
+                    active-class="is-active"
+                >
+                    Notes
+                </RouterLink>
+                <RouterLink 
+                    to="/stats"
+                    class="navbar-item"
+                    active-class="is-active"
+                >
+                    Stats
+                </RouterLink>
             </div>
-          </div>
-      
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <a class="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a class="button is-light">
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
-      </nav>
+    </nav>
 </template>
