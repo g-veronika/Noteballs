4e25166bc1f53c3676d9fff5750b95b24a747765
g-veronika <g.veronika2021@gmail.com>
2023-01-01 18:07:55 +0100

    computed (Note length)

--- vue-project/src/components/Notes/Note.vue
+++ vue-project/src/components/Notes/Note.vue
@@ -5,6 +5,11 @@
         <div class="card-content">
             <div class="content">
                 {{ note.content }}
+                <div class="has-text-right has-text-grey-light mt-2">
+                    <small>
+                        {{ characterLength }}
+                    </small>
+                </div>
             </div>
         </div>
         <footer class="card-footer">
@@ -16,11 +21,20 @@
 
 <script setup> 
 
+    import { computed } from 'vue';
+
     const props = defineProps({
         note: {
             type: Object,
             required: true
         }
+    })
+
+    const characterLength = computed(() => {
+
+        let length = props.note.content.length
+        let description =  length > 1 ? 'characters' : 'character'
+        return `${ length }  ${ description }` 
     })
 
 </script>
