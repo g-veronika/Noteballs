cdb58c86510ee21cb76af7310a9c2610a2a0c86f
g-veronika <g.veronika2021@gmail.com>
2023-01-03 17:31:26 +0100

    view edit note add

--- vue-project/src/views/ViewEditNote.vue
+++ vue-project/src/views/ViewEditNote.vue
@@ -1,5 +1,26 @@
 <template>
     <div class="edit-note">
-        <h1>Edit Note with an id of: {{ $route.params.id }}</h1>
+        <AddEditNote
+            v-model="noteContent"
+            ref="addEditNoteRef"
+        >
+            <template #buttons>
+                <button 
+                    class="button is-link has-background-success"
+                >
+                    Add New Note 
+                </button>
+            </template>
+    </AddEditNote>
     </div>
 </template>
+
+<script setup>
+
+    import { ref } from 'vue';
+    import AddEditNote from '../components/Notes/AddEditNote.vue';
+
+    const noteContent = ref('test')
+    const addEditNoteRef = ref()
+    
+</script>
