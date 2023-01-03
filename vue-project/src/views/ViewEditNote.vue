a7ca2332d5ee3fcc0b51ccbd6738e8346c6987a7
g-veronika <g.veronika2021@gmail.com>
2023-01-03 23:33:40 +0100

    set up getter for content id

--- vue-project/src/views/ViewEditNote.vue
+++ vue-project/src/views/ViewEditNote.vue
@@ -9,7 +9,7 @@
         >
             <template #buttons>
                 <button
-                    @click="$router.push('/')" 
+                    @click="$router.back()" 
                     class="button is-link is-light"
                 >
                     Cancel
@@ -29,9 +29,18 @@
 <script setup>
 
     import { ref } from 'vue';
+    import { useRoute } from 'vue-router'
     import AddEditNote from '../components/Notes/AddEditNote.vue';
+    import { useStoreNotes } from '@/stores/storeNotes'
 
+    const storeNotes = useStoreNotes()
+
+    const route = useRoute()
+ 
     const noteContent = ref('')
-    const addEditNoteRef = ref()
-    
+    console.log(route.params.id);
+    // const addEditNoteRef = ref()
+
+    noteContent.value = storeNotes.getNoteContent
+
 </script>
