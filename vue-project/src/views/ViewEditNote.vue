4ad90dd831c601aeb530ecf6bc695ab3f1b2a5e1
g-veronika <g.veronika2021@gmail.com>
2023-01-03 14:07:12 +0100

    edit note page & route

--- vue-project/src/views/ViewEditNote.vue
+++ vue-project/src/views/ViewEditNote.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="edit-note">
-        <h1>Edit Note with an id of: {{ $route.params.id }} </h1>
+        <h1>Edit Note with an id of: {{ $route.params.id }}</h1>
     </div>
 </template>
--- vue-project/src/views/ViewNotes.vue
+++ vue-project/src/views/ViewNotes.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="notes">
 
-        <div class="card has-background-success-dark p-4 mb-5">
+        <AddEditNote>
+            <template #buttons>
+                <button 
+                @click="addNote"
+                :disabled="!newNote"
+                class="button is-link has-background-success"
+            >
+                Add New Note
+            </button>
+            </template>
+        </AddEditNote>
+        <!-- <div class="card has-background-success-dark p-4 mb-5">
             <div class="field">
                 <div class="control">
                     <textarea 
@@ -24,7 +35,7 @@
                     </button>
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <Note
             v-for="note in storeNotes.notes"
@@ -38,6 +49,7 @@
 
     import { ref } from 'vue';
     import Note from '@/components/Notes/Note.vue'
+    import AddEditNote from '@/components/Notes/AddEditNote.vue'
     import { useStoreNotes } from '@/stores/storeNotes'
 
     const storeNotes = useStoreNotes()    
