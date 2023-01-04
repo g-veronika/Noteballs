489ce5a6079fa5dcc3bf30915188472a6185a7e5
g-veronika <g.veronika2021@gmail.com>
2023-01-04 16:17:05 +0100

    click outside composable

--- vue-project/src/components/Layout/NavBar.vue
+++ vue-project/src/components/Layout/NavBar.vue
@@ -18,6 +18,7 @@
                     aria-label="menu" 
                     aria-expanded="false" 
                     data-target="navbarBasicExample"
+                    ref="navbarBurgerRef"
                 >
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -29,6 +30,7 @@
                 id="navbarBasicExample" 
                 class="navbar-menu"
                 :class="{ 'is-active' : showMobileNav }"
+                ref="navbarMenuRef"
             >
                 <div class="navbar-end">
                     <RouterLink 
@@ -56,8 +58,19 @@
 <script setup>
 
     import { ref } from 'vue';
+    import { onClickOutside } from '@vueuse/core'
 
     const showMobileNav = ref(false)
+
+    const navbarMenuRef = ref(null)
+
+    const navbarBurgerRef = ref(null)
+
+    onClickOutside(navbarMenuRef, () => {
+        showMobileNav.value = false
+    }, {
+        ignore: [navbarBurgerRef]
+    })
 
 </script>
 
