9defe90660b907ff84b750c404fe38b53c21284b
g-veronika <g.veronika2021@gmail.com>
2023-01-05 16:22:18 +0100

    hide the delete modal

--- vue-project/src/components/Notes/ModalDeleteNote.vue
+++ vue-project/src/components/Notes/ModalDeleteNote.vue
@@ -4,15 +4,41 @@
         <div class="modal-card">
           <header class="modal-card-head">
             <p class="modal-card-title">Delete Note</p>
-            <button class="delete" aria-label="close"></button>
+            <button 
+                @click="closeModal"
+                class="delete" aria-label="close"
+            >
+            </button>
           </header>
           <section class="modal-card-body">
             Are you sure you want to delete this note?
           </section>
           <footer class="modal-card-foot">
-            <button class="button">Cancel</button>
+            <button 
+                @click="closeModal"
+                class="button"
+            >
+                Cancel
+            </button>
             <button class="button is-danger">Delete Note</button>
           </footer>
         </div>
       </div>
 </template>
+
+<script setup>
+
+    const props = defineProps({
+        modelValue: {
+            type: Boolean,
+            default: false
+        }
+    })
+
+    const emit = defineEmits(['update:modelValue'])
+
+    const closeModal = () => {
+        emit('update:modelValue', false)
+    }
+
+</script>
